57d43beafb52c18894611e64154499e703bf2d96
manhndhe161791 <manhndhe161791@fpt.edu.vn>
2023-03-26 16:09:51 +0700

    CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,4 @@
-import e from "express";
+// import express from "express";
 import pool from "../configs/connectDB";
 
 let getHomepage = async (req, res) => {
@@ -18,7 +18,22 @@ let createNewUser = async (req, res) => {
 
     return res.redirect('/');
 }
-
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId;
+    await pool.execute('DELETE FROM user WHERE id = ?', [userId]);
+    return res.redirect('/');
+}
+let getEditPage = async (req, res) => {
+    let id = req.params.id;
+    let [user] = await pool.execute('SELECT * FROM user WHERE id = ?', [id])
+    return res.render(`updateUser.ejs`, { dataUser: user[0] });
+}
+let updateUser = async (req, res) => {
+    let { id, firstName, lastName, email, address } = req.body;
+    await pool.execute('UPDATE user SET firstName = ?, lastName = ?, email = ?, address = ? WHERE id = ?',
+        [firstName, lastName, email, address, id]);
+    return res.redirect('/')
+}
 module.exports = {
-    getHomepage, getDetailPage, createNewUser
+    getHomepage, getDetailPage, createNewUser, deleteUser, getEditPage, updateUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -7,6 +7,9 @@ const initWebRoute = (app) => {
     route.get('/', homeController.getHomepage);
     route.get('/detail/user/:id', homeController.getDetailPage);
     route.post('/creat-new-user', homeController.createNewUser);
+    route.post('/delete-user', homeController.deleteUser);
+    route.get('/edit-user/:id', homeController.getEditPage)
+    route.post('/update-user', homeController.updateUser)
     route.get('/miku', (req, res) => {
         res.send(`Do you wanna listen a song?`)
     })
